95f017c53f035a75b089fedf26384ad06c3d9225
Aaron Westbrook <westbrook823@gmail.com>
2019-12-31 11:10:41 -0500

    Install sass-loader, bootstrap, node-sass

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,8 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import css from "../css/app.css"
+// import css from "../css/app.css"
+import "../scss/app.scss";
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -29,6 +29,17 @@ module.exports = (env, options) => ({
         }
       },
       {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader']
       }
